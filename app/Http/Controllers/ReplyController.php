1ab787329b2d290d28e73658df330b928c081d66
mohamedkhalil1 <mohamedkhalil125445@gmail.com>
2020-03-04 07:54:13 +0200

    JWT Middleware and Exception hander done

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -9,6 +9,16 @@ use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
+
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('jwt')->except(['index','show']);
+    }
     /**
      * Display a listing of the resource.
      *
