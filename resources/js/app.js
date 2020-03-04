1cd105429ad0190433bbd43f9a8d430a44bace79
mohamedkhalil1 <mohamedkhalil125445@gmail.com>
2020-03-04 10:22:24 +0200

    vuetify , vueRouter , Initial Componemt

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,11 @@
 
 require('./bootstrap');
 
+
+
 window.Vue = require('vue');
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,13 +24,18 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('AppHome', require('./components/AppHome.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const vuetify = new Vuetify();
 
+import router from './Router/router.js'
 const app = new Vue({
     el: '#app',
+    vuetify,
+    router
 });
